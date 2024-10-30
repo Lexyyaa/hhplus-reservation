84d6af9e6677e5b1b21a8b3b719e943050e55bda
Lexyyaa <143318360+Lexyyaa@users.noreply.github.com>
2024-10-30 20:44:39 +0900

    Update GlobalExceptionHandler.java

--- reservation/src/main/java/com/hhplus/reservation/support/error/GlobalExceptionHandler.java
+++ reservation/src/main/java/com/hhplus/reservation/support/error/GlobalExceptionHandler.java
@@ -10,20 +10,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BizException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(BizException ex) {
-//        // error log handling
-//        switch (e.getErrorType().getLogLevel()) {
-//            case Error -> log.error();
-//            case Warn -> log.warn();
-//            default -> log.info();
-//        }
-//        // HTTP STATUS CODE MAPPING
-//        switch (e.getErrorType().getErrorCode()) {
-//            --->
-//            case DB_ERROR, KAKA_ERROR -> 500;
-//            case CLIENT_ERROR -> 400;
-//            case NOT_FOUND -> 404;
-//            default -> HttpStatus .200; ( 200 Error 표현하는 케이스 )
-//        }}
         return ErrorResponse.toResponseEntity(ex.getErrorType());
     }
 
@@ -32,5 +18,3 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ErrorResponse.toResponseEntity(ErrorType.INTERNAL_SERVER_ERROR);
     }
 }
-
-//https://github.com/hyunn12/hhplus-server-reservation/pull/21
