a97e7657328005058149492cbdb309e9ce9e8cff
Lexyyaa <shw9910274@gmail.com>
2024-11-01 06:43:19 +0900

    [refactor] - 좌석예약 코드수정(낙관적 락 적용), 트랜잭션범위 수정

--- reservation/src/main/java/com/hhplus/reservation/domain/reserve/ReservationService.java
+++ reservation/src/main/java/com/hhplus/reservation/domain/reserve/ReservationService.java
@@ -5,16 +5,21 @@ import com.hhplus.reservation.domain.concert.ConcertRepository;
 import com.hhplus.reservation.domain.concert.ConcertSeatStatus;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final ConcertRepository concertRepository;
+    private final RedissonClient redissonClient;
 
     /**
      * 좌석을 예약한다.
@@ -30,36 +35,61 @@ public class ReservationService {
         return Reservation.convert(reservation);
     }
 
+    public ReservationInfo reserveWithDLock(Long concertScheduleId, Long userId, Long totalPrice, List<Long> seats){
+        String lockKey = "reservationLock:" + concertScheduleId + ":" + seats.hashCode();
+        RLock lock = redissonClient.getLock(lockKey);
+
+        try {
+            if (lock.tryLock(5, 10, TimeUnit.SECONDS)) {
+                log.info("Reservation lock start");
+                try {
+                    Reservation currReservation = Reservation.create(userId, concertScheduleId, totalPrice, ReservationType.TEMP_RESERVED);
+                    Reservation reservation = reservationRepository.save(currReservation);
+
+                    List<ReservationSeat> currReservationSeats = ReservationSeat.createList(reservation.getId(), seats);
+                    reservationRepository.saveAll(currReservationSeats);
+
+                    return Reservation.convert(reservation);
+                } finally {
+                    log.info("Reservation unLock");
+                    lock.unlock();
+                }
+            } else {
+                throw new IllegalStateException("락 획득 실패");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException("인터럽트 발생", e);
+        }
+    }
+
     /**
      * 예약정보를 반환한다.
      */
     @Transactional
     public ReservationInfo getReservation(long reservationId) {
-        Reservation reservation = reservationRepository.findByIdWithLock(reservationId);
+        Reservation reservation = reservationRepository.findById(reservationId);
         return Reservation.convert(reservation);
     }
 
     /**
      * 예약을 확정한다.
      */
+    @Transactional
     public ReservationInfo confirmedReservation(ReservationInfo reservation){
-
-        Reservation currReservation = Reservation.create(reservation);
-        currReservation.setReserved(ReservationType.RESERVED);
-        currReservation.setReservedAt(LocalDateTime.now());
-
-        Reservation saveReservation = reservationRepository.save(currReservation);
+        Reservation saveReservation = reservationRepository.confirmedReservationWithLock(reservation.getId());
         return Reservation.convert(saveReservation);
     }
 
     /**
      * 예약내역을 복원한다(예약만료)
      */
+    @Transactional
     public void restoreReservation() {
         List<Reservation> expiredReservations = reservationRepository.findExpiredReservation();
 
         for (Reservation reservation : expiredReservations) {
-            List<Long> seatIds = reservationRepository.findByReservationId(reservation.getId());
+            List<Long> seatIds = reservationRepository.findSeatsByReservationId(reservation.getId());
 
             concertRepository.updateSeatsAvaliable(seatIds, ConcertSeatStatus.AVAILABLE);
 
--- reservation/src/main/java/com/hhplus/reservation/infra/reservation/JPAReservationRepository.java
+++ reservation/src/main/java/com/hhplus/reservation/infra/reservation/JPAReservationRepository.java
@@ -4,7 +4,6 @@ import com.hhplus.reservation.domain.reserve.Reservation;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,10 +12,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface JPAReservationRepository extends JpaRepository<Reservation, Long> {
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT r FROM Reservation r WHERE r.id = :reservationId")
     Optional<Reservation> findByIdWithLock(@Param("reservationId") Long reservationId);
 
     @Query("SELECT r FROM Reservation r WHERE r.reserveExpiredAt < :currTime AND r.reserved = 'TEMP_RESERVED'")
     List<Reservation> findExpiredReservation(@Param("currTime") LocalDateTime currTime);
+
+    @Query("SELECT r FROM Reservation r WHERE r.id = :reservationId")
+    Optional<Reservation> findByReservationId(@Param("reservationId") Long reservationId);
+
 }
