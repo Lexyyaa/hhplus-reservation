39ce29b4a002aceeff5fdeded2ff7c84efaf9df8
Lexyyaa <shw9910274@gmail.com>
2024-11-23 02:12:39 +0900

    [fix] - @Transactional 추가

--- reservation/src/main/java/com/hhplus/reservation/domain/message/MessageOutboxService.java
+++ reservation/src/main/java/com/hhplus/reservation/domain/message/MessageOutboxService.java
@@ -3,6 +3,7 @@ package com.hhplus.reservation.domain.message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,22 +18,21 @@ public class MessageOutboxService {
      * 아웃박스메시지를 저장한다.
      */
     public MessageOutbox save(MessageOutbox messageOutbox) {
-        log.info("=================== Save message outbox: {}", messageOutbox.getEventId());
         return messageOutboxRepository.save(messageOutbox);
     }
 
     /**
      * 메시지를 발행상태로 변경한다.
      */
+    @Transactional
     public void chkPublished(Long messageKey,String type){
-        log.info("=================== chkPublished messageKey={},type={}", messageKey, type);
         MessageOutbox messageOutbox = messageOutboxRepository.findByEventIdAndEventType(messageKey,type);
         messageOutbox.published();
         messageOutboxRepository.save(messageOutbox);
     }
 
     /**
-     * 실패한 메시지를 재발행한다.
+     * 재실행할 메시지 목록을 가져온다.
      */
     public List<MessageOutbox> findRetryMessages(){
         return messageOutboxRepository.findRetryMessages();
